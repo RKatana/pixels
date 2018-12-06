f251b6fff598e5f62aff5535ba4d7df507afae33
Marvin Chomba <marvin.chomba24@gmail.com>
2018-12-06 12:06:27 +0300

    Add the upload functionality

--- photos/views.py
+++ photos/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse,Http404
+from .models import Image,Location,Category
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hey")
+
+    context = {"images":Image.get_images()}
+
+    return render(request,"index.html",context)
