ddee3f755e702e9bc868b1d3aef538d96c255643
Marvin Chomba <marvin.chomba24@gmail.com>
2018-12-06 11:54:21 +0300

    Add the class methods

--- photos/models.py
+++ photos/models.py
@@ -7,16 +7,51 @@ class Location(models.Model):
     """
     name = models.CharField(max_length = 30)
 
+    def save_location(self):
+        """
+        This is the function that we will use to save the instance of this class
+        """
+        self.save()
+
+    def delete(self):
+        """
+        This is the method to delete the instance
+        """
+        Location.objects.get(id = self.id).delete()
+
+    def update(self,field,val):
+        """
+        This is the method to update the instance
+        """
+        Location.objects.get(id = self.id).update(field = val)
+
 
 class Category(models.Model):
     """
     This is the class where we will create categories
     """
     name = models.CharField(max_length = 30)
+    def save_location(self):
+        """
+        This is the function that we will use to save the instance of this class
+        """
+        self.save()
+
+    def delete(self):
+        """
+        This is the method to delete the instance
+        """
+        Location.objects.get(id = self.id).delete()
+
+    def update(self,field,val):
+        """
+        This is the method to update the instance
+        """
+        Location.objects.get(id = self.id).update(field = val)
 
 class Image(models.Model):
     """
-    This is the class we will use toc reate images
+    This is the class we will use to create images
     """
     image_url = models.ImageField(upload_to = "images/")
     name = models.CharField(max_length = 30)
@@ -24,5 +59,42 @@ class Image(models.Model):
     location = models.ForeignKey(Location)
     category = models.ForeignKey(Category)
 
+    def save_image(self):
+        """
+        This is the function that we will use to save the instance of this class
+        """
+        self.save()
 
+    def delete_image(self):
+        """
+        This is the function that we will use to delete the instance of this class
+        """
+        Image.objects.get(id = self.id).delete()
+
+    def update_image(self,field,val):
+        """
+        This is the method to update the instance
+        """
+        Image.objects.get(id = self.id).update(field = val)
+
+    @classmethod
+    def get_image_by_id(cls,id):
+        """
+        This is the method to get a specific image
+        """
+        return cls.objects.get(id = id)
+
+    @classmethod
+    def search_image(cls,category):
+        """
+        This is the method to search images based on a specific category
+        """
+        return cls.objects.filter(category__icontains = category)
+
+    @classmethod
+    def filter_by_location(cls,location):
+        """
+        This is the method to get images taken in a certain location
+        """
+        return cls.objects.filter(location = location)
 
