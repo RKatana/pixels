8070ee674f27df2069f8e0d61c90455b2d463491
Marvin Chomba <marvin.chomba24@gmail.com>
2018-12-06 20:34:48 +0300

    Style the main form

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,6 +1,7 @@
 $(window).scroll(() => {
     if ($(document).scrollTop() > 450) {
         $("nav").addClass("shrink")
+        $(".nav-form").show()
         
     } else  {
         $("nav").removeClass("shrink")
