ec2689fb03491d2796accf1ec9037000cfd9fd6d
Marvin Chomba <marvin.chomba24@gmail.com>
2018-12-08 10:07:00 +0300

    Add view btn

--- static/js/modal.js
+++ static/js/modal.js
@@ -2,3 +2,11 @@ showModal = (i) => {
     $("#myModal").modal("show")
     $(".mod-img").attr("src",i)
 }
+$(document).ready(() => {
+    $(".image").mouseover(() => {
+        alertI("Hey")
+    })
+})
+showButton = () => {
+    $(".modal-btn").show()
+}
